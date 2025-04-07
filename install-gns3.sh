006da5da81eb53e3c91cd8d50516e86f3aae5c19
Bastian <153897299+Bastiantkt@users.noreply.github.com>
2025-04-07 21:40:17 +0200

    Add files via upload

---
+++ install-gns3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "installing required packages..."
+yay -Sy --noconfirm
+yay -S --noconfirm gns3-server gns3-gui dynamips ubridge qemu docker wireshark-qt vpcs libvirt gperftools tigervnc
+
+echo "Adding user to required groups..."
+sudo usermod -aG docker,wireshark,kvm,libvirt $(whoami)
+
+echo "Enabling and starting systemd services..."
+sudo systemctl enable --now docker
+sudo systemctl enable --now libvirtd
+sudo virsh net-autostart default
+
+echo "GNS3 installation completed! Please reboot your system for the changes to take effect."
