0740d050b7455b6725b775c37ea833cae0463463
Bastian <153897299+Bastiantkt@users.noreply.github.com>
2025-04-07 21:41:53 +0200

    Update install-gns3.sh

--- install-gns3.sh
+++ install-gns3.sh
@@ -2,7 +2,7 @@
 
 echo "installing required packages..."
 yay -Sy --noconfirm
-yay -S --noconfirm gns3-server gns3-gui dynamips ubridge qemu docker wireshark-qt vpcs libvirt gperftools tigervnc
+yay -S --noconfirm gns3-server gns3-gui dynamips ubridge qemu docker wireshark-qt vpcs libvirt gperftools tigervnc inetutils
 
 echo "Adding user to required groups..."
 sudo usermod -aG docker,wireshark,kvm,libvirt $(whoami)
